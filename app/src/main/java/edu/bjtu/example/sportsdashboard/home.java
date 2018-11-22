9a17d8b68b6dc8f6fc434ba08de3dd6359a6901b
ZZH <17735679828@163.com>
2018-11-22 18:03:19 +0800

    First commit(第一次提交)

--- app/src/main/java/edu/bjtu/example/sportsdashboard/home.java
+++ app/src/main/java/edu/bjtu/example/sportsdashboard/home.java
@@ -118,19 +118,12 @@ public class home extends AppCompatActivity
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.left_navigation, menu);
-//        return true;
-//    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
 //        if (id == R.id.action_settings) {
