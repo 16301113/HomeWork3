c94667553202790c473be64ea704fa6255ccc79e
ZZH <17735679828@163.com>
2018-11-22 16:58:26 +0800

    Second push

--- app/src/main/java/edu/bjtu/example/sportsdashboard/MainActivity.java
+++ app/src/main/java/edu/bjtu/example/sportsdashboard/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.bjtu.example.sportsdashboard;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 //import android.app.Fragment;
@@ -13,91 +14,122 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Gravity;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.sports.sportclub.DataModel.User;
+import edu.bjtu.example.sportsdashboard.MainActivity;
+import edu.bjtu.example.sportsdashboard.register;
+import edu.bjtu.example.sportsdashboard.home;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobUser;
+import com.sports.sportclub.api.BmobService;
+import com.sports.sportclub.api.Client;
 
 import static android.support.v4.view.GravityCompat.*;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+//轮播
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import java.util.ArrayList;
+import android.widget.LinearLayout;
+
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 
 public class MainActivity extends AppCompatActivity{
+    private BmobUser current_user;
     private DrawerLayout drawerLayout;
+    private SliderLayout sliderShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Bmob.initialize(this, "24d2dd9a00667b645f55acaef2d11e16");
+        current_user = BmobUser.getCurrentUser();
+        if (current_user != null) {
+            jump2main();
+        } else {
+            //设置下划线
+            TextView forget_text = findViewById(R.id.forget_text);
+            forget_text.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+            //设置监听
+            forget_text.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "该功能未开放", Toast.LENGTH_LONG).show();
+                }
+            });
 
-
-
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        drawer = findViewById(R.id.drawer_layout);
-//        NavigationView navigationView = findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
-//
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-//                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
-
-        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
-//                    new DashboardFragment()).commit();
-            //navigationView.setCheckedItem(R.id.nav_home);
+            TextView signup_text = findViewById(R.id.register_text);
+            signup_text.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+            signup_text.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, register.class);
+                    startActivity(intent);
+                }
+            });
         }
     }
 
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.nav_home:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
-//                        new DashboardFragment()).commit();
-//                break;
-//            case R.id.nav_info:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
-//                        new AnnounceFragment()).commit();
-//                break;
-//            case R.id.nav_schedule:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
-//                        new ScheduleFragment()).commit();
-//                break;
-//            case R.id.nav_coachlist:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
-//                        new CoachListFragment()).commit();
-//                break;
-//
-//            case R.id.nav_share:
-//                Toast.makeText(this, "Share menu clicked ", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.nav_send:
-//                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
-//                break;
-//        }
-//
-//        drawer.closeDrawer(START);
-//        return true;
-//    }
-//
-//    @Override
-//    public void onBackPressed() {
-//        if (drawer.isDrawerOpen(START)) {
-//            drawer.closeDrawer(START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
+    public void Login(View view) {
+        EditText username_input = findViewById(R.id.username_input);
+        EditText password_input = findViewById(R.id.password_input);
 
-    public void DengLu(View view)
-    {
+        final String username = username_input.getText().toString();
+        String password = password_input.getText().toString();
+
+        //使用retrofit实现登录请求
+        BmobService service = Client.retrofit.create(BmobService.class);
+        Call<ResponseBody> call = service.getUser(username,password);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                if(response.code() == 200){
+                    showmsg("登陆成功");
+                    jump2main();
+                }
+                else if(response.code() == 400) {
+                    showmsg("用户名或密码错误");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                showmsg(t.getMessage());
+            }
+        });
+    }
+
+    public boolean Validation(User user){
+
+        return false;
+    }
+    public void showmsg(String str){
+        Toast.makeText(MainActivity.this,str,Toast.LENGTH_LONG).show();
+    }
+
+    //跳转至主界面
+    public void jump2main(){
         Intent intent = new Intent(this,home.class);
         startActivity(intent);
+        finish();
+    }
 
-    }
-    public void ZhuCe(View view)
-    {
-        Intent intent = new Intent(this,register.class);
-        startActivity(intent);
-    }
+
 }
